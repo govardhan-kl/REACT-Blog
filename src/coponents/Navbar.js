146f84139d9a7fe1cb75679c8e71ee821d5b7fcc
Govardhana_KL <178376@ust-global.com>
2023-10-04 18:00:17 +0530

    Implemente Navbar

--- src/coponents/Navbar.js
+++ src/coponents/Navbar.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
     return (
-      <div className="">
-        
+      <div>
+        <ul id="nav">
+          {/* Link is coming from react-router-dom */}
+          <li><Link to="/">Home</Link> </li>
+          <li><Link to="/post/:postid">View Blog</Link> </li>
+          <li><Link to="/create-post">Create Blog</Link> </li>
+        </ul>
       </div>
     );
   }
