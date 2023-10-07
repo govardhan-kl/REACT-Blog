abe239c158080b97934f51591ab187a58c622176
Govardhana_KL <178376@ust-global.com>
2023-10-07 20:26:30 +0530

    Impleneted Styled-Components

--- src/coponents/Home.js
+++ src/coponents/Home.js
@@ -2,6 +2,34 @@ import { collection, onSnapshot, query } from "firebase/firestore";
 import {db} from '../firebase';
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+//using style componenets
+import styled from "styled-components";
+
+// this is now a COMPONENT
+const SubTitle = styled.div`
+  color:grey
+`;
+
+const Post = styled.div`
+  border: 1px solid #e1e1e1;
+  background:lightgrey;
+
+  &:hover{
+    border: 1px solid #2196f3;
+  }
+
+  p {
+    color:red;
+  }
+
+  @media (max-width:800px){
+    border-top: 1px solid black;
+    p {
+      color:yellow;
+    }
+  }
+`;
+
 
 function Home() {
 
@@ -25,15 +53,15 @@ function Home() {
   return (
     <div className="home">
       <h1 style={styles.heading}>Tech Blogs</h1>{/* INLINE STYING*/}
-      <div id="blog-by" >Govardhana</div>
+      <SubTitle id="blog-by" >Govardhana</SubTitle>{/* STYLED COMPONENET WAY*/}
 
       {posts.map((post,index)=>(
-        <div className="post" key={`post-${index}`}>
+        <Post className="post" key={`post-${index}`}>
           <Link to={`/post/${post.id}`}>
             <h3>{post.title}</h3>
           </Link>
           <p style={{fontStyle:"italic"}}>{post.subtitle}</p>{/* INLINE STYING*/}
-        </div>
+        </Post>
       ))}
     </div>
   );
